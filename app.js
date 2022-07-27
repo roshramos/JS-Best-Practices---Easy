b78360636280df053d5d36690defaaf7bf3f20fb
roshramos <rosh.ramos30@hotmail.com>
2022-07-27 18:05:05 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+//  function addition (num1, num2) {
+//     return num1 + num2 
+// }
+
+// console.log (addition(3, 5))
+
+// function hoursIntoSeconds (hour) {
+//     return hour * (60 * 60)
+// }
+
+// console.log (hoursIntoSeconds(24))
+
+// function rectanglePerimeter (length, widht) {
+//     return (2 * length) + (2 * widht);
+// }
+
+// console.log (rectanglePerimeter(6, 7))
+
+// function calcTriangleArea (base, height) {
+//     return 0.5 * base * height 
+// }
+
+// console.log (calcTriangleArea(10, 10))
+
+// function appendFrontend(str) {
+//     return str + 'Frontend'
+// }
+
+// console.log(appendFrontend('Apple'))
+
+// function sumGreaterThan100(num1, num2) {
+//     return num1 + num2 > 100
+// }
+
+// console.log (sumGreaterThan100(50, 49))
+
+// function lessThanOrEqualToZero (num1) {
+//     return num1 <= 0;
+// }
+
+// console.log (lessThanOrEqualToZero(1))
+
+// function oppositeBooelan (boolean) {
+//     return !boolean;
+// }
+
+// console.log (oppositeBooelan(true))
+
+/**NOTE: IF ! IS ADDED TO A BOOLEAN IT WILL RETURN THE OPPOSITE
+ * VALUE OF THE GIVEN BOOELEAN VALUE
+**/
+
+function isNotZero (element) {
+    return element !== 0
+}
+
+console.log (isNotZero(5))
