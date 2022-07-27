5aced579c492d167f62107c0e1ba0540678d49a7
roshramos <rosh.ramos30@hotmail.com>
2022-07-27 18:53:48 +1000

    Finished the easy best practices

--- app.js
+++ app.js
@@ -50,8 +50,57 @@
  * VALUE OF THE GIVEN BOOELEAN VALUE
 **/
 
-function isNotZero (element) {
-    return element !== 0
+// function isNotZero (element) {
+//     return element !== 0
+// }
+
+// console.log (isNotZero(5))
+
+// function calcRemainder(num1, num2) {
+//     return num1 % num2;
+// }
+
+// console.log (calcRemainder(10, 9))
+
+// function isOdd(num1) {
+//     return num1 % 2 !== 0
+// }
+
+// console.log (isOdd(121))
+
+// function booleanInteger(num) {
+//     return num % 2 === 0 ? 1 : -1;
+// }
+
+// console.log (booleanInteger(2))
+
+/**NOTE: THE TURNERY OPERATOR "?" ONLY CHECKS IF THE VALUE IS TRUTHY AND NOT THE BOOLEAN TRUE
+ * E.G num % 2 === 0 ? 1 : -1;
+ * If the value of the left side is true then it return what's on the left side "1"
+ * If its false then it returns what's on the right side "-1"
+ * 
+ * Extra Examples
+ * 
+ * 5 ? true: false = true
+ * false ? true : flase = false
+ * '' ? true : false = false
+ * 0 ? true : false = false
+ * 
+ * It is similar to a truthy statement, it doesn't check if the variable is exactly the true variable, it just checks if it's truthy or falsy
+ */
+
+function isLoggedInAndSubscribed (str1, str2) {
+    return str1 === 'LOGGED_IN' && str2 === 'SUBSCRIBED'
+    return str1 === 'LOGGED_IN' || str2 === 'SUBSCRIBED'
 }
 
-console.log (isNotZero(5))
+console.log (isLoggedInAndSubscribed('LOGGED_IN', 'SUBSCRIBED'))
+
+/**If something and something needs to happen you should straight away use &&
+ * But if something is asking for "or" use ||
+ * 
+ * TO CLARIFY
+ * IF YOU NEED TO BE LOGGED IN AND SUBSCRIBED TO ENTER USE &&
+ * BUT IF YOU ONLY NEED TO BE LOGGED IN OR SUBSCRIBED TO ENTER USE ||
+ */
+
